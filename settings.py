ed49881b5ffc1e55be147300ffae52378d1ad25c
oleg <gusevoleg96@gmail.com>
2023-11-03 22:11:47 +0400

    test

---
+++ settings.py
@@ -0,0 +1,13 @@
+domain = "https://api.glassen-it.com/"
+auth_url = f"{domain}component/socparser/authorization/login"
+post_url = f"{domain}component/socparser/content/posts"
+
+login = "java_api"
+password = "4yEcwVnjEH7D"
+thread_id = 18514
+referenceFilter = []
+bot_data = "6863052268:AAEzmR1ZZVznD94VQWutuNvHN084jo4ZC-M"
+chart_id = "-4077968276"
+UTC = 3
+FROM_MINUTES = 2500
+POSTS_LIMIT = 5_000
